5a35f53af8571c72815c0cbdbf0d75507398d284
Math9751 <matthewbelt.mc@gmail.com>
2026-07-08 11:27:09 -0300

    estilizando os botões

--- app/src/main/java/com/example/netcheckapp/MainActivity.kt
+++ app/src/main/java/com/example/netcheckapp/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.netcheckapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.netcheckapp.ui.theme.NetCheckAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,12 +50,17 @@ fun TelaPrincipalContent(
     statusRede: String,
     onEnviarClick: () -> Unit
 ) {
-    // Componente de texto na tela
-    Text(text = "Status: $statusRede")
+    Column(modifier = Modifier.padding(16.dp)) {
+        // Componente de texto na tela
+        Text(text = "Status: $statusRede")
 
-    // Componente de botão na tela
-    Button(onClick = onEnviarClick) {
-        Text("Enviar Dados ao Servidor")
+        // Componente de botão na tela
+        Button(
+            onClick = onEnviarClick,
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text("Enviar Dados ao Servidor")
+        }
     }
 }
 
